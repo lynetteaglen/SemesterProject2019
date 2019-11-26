6de72c214608d61b1b83762892201029433a09a3
Lynette Aglen <lynette.aglen@gmail.com>
2019-11-26 21:39:05 +0100

    boardgame major changes

--- boardgame.js
+++ boardgame.js
@@ -8,18 +8,18 @@ console.log(sessionStorage.getItem("player"));
 // Player objects with variables and values. 
 let playerOne = {
   tile: 0,
+  id: 1,
   PlayerName: "",
   isPlayerTurn: true,
   playerImg: `/Images/${playerImage}.svg`,
-  itsMe: true
 };
 
 let playerTwo = {
   tile: 0,
+  id: 2,
   PlayerName: "",
   isPlayerTurn: false,
   playerImg: `/Images/${playerImage}.svg`,
-  itsMe: false
 };
 
 
@@ -33,39 +33,65 @@ document.getElementById(`tile_0`).innerHTML += `<img src="${playerTwo.playerImg}
 function rollDiceAndMovePlayer() { // Find current player.
 
   // Populate empty object with either playerOne or playerTwo depending on whos turn it is.
-  let currentPlayer = {};
+  let currentPlayer = getCurrentPlayer();
+  // This will display the value that currentPlayer contains. 
+  console.log("The current player is: " + currentPlayer.id);
 
+  let diceRoll = rollDice();
+
+  movePlayer(diceRoll, currentPlayer);
+
+  //switching characters
+  currentPlayer = changeTurn(currentPlayer);
+}
+
+
+// finds out if player one or player two is the current player
+// returns the current player 
+function getCurrentPlayer() {
+  let currentPlayer = {};
   if (playerOne.isPlayerTurn === true) {
     currentPlayer = playerOne;
   }
   else {
     currentPlayer = playerTwo;
   }
+  return currentPlayer;
+}
 
 
+function rollDice() {
   // Makes a variable named diceRoll. Contains a method that makes the dice span from 1-6 (whole numbers ->floor). 
   let diceRoll = (Math.floor(Math.random() * 6) + 1)
-  // This will display the value that currentPlayer contains. 
-  console.log(currentPlayer);
+  console.log("The number of steps is: " + diceRoll);
+  return diceRoll;
+}
+
+
+
+function movePlayer(diceRoll, currentPlayer) {
   // Change the players turn back when the dice rolls 6. If not rolled 6, display the player on the tile. 
   if (diceRoll === 6) {
+    // moves 6 
+    document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}" class="hide" width="60px" border-radius="50px"/>`;
     console.log('Have another roll!');
-
-    rollDiceAndMovePlayer();
+    diceRoll = rollDice();
+    movePlayer(diceRoll, currentPlayer)
   }
   else {
     document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}" class="hide" width="60px" border-radius="50px"/>`;
   }
-
-
   // Gets the objects and tile and updates the tile with the result of the diceRoll. 
   currentPlayer.tile = currentPlayer.tile + diceRoll;
   console.log(currentPlayer.tile);
 
   // Placing the information from object inside the HTML. Then adds the image gotten from the previous page. 
   document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}">`
+}
 
-  //switching characters
+
+
+function changeTurn(currentPlayer) {
   if (currentPlayer === playerOne) {
     playerOne.isPlayerTurn = false;
     playerTwo.isPlayerTurn = true;
@@ -73,8 +99,8 @@ function rollDiceAndMovePlayer() { // Find current player.
     playerOne.isPlayerTurn = true;
     playerTwo.isPlayerTurn = false;
   }
-
-
+  return currentPlayer;
 }
+
 
 
