0d55d55ba35bb77bb394dbfebd3ce83aee970003
Lynette Aglen <lynette.aglen@gmail.com>
2019-12-01 15:37:01 +0100

    dont want to lose this

--- boardgame.js
+++ boardgame.js
@@ -49,11 +49,13 @@ function boardFunction() {
     whichPlayer.tile += diceRoll;
 
     document.getElementById('tile_' + whichPlayer.tile).innerHTML += `<img src="${whichPlayer.playerImg}" width="60px"/>`;
+    window.alert("The Gods are with you. Continue your journey!");
     console.log('The Gods are with you. Continue your journey!');
     boardFunction();
   }
   else {
-    document.getElementById('tile_' + whichPlayer.tile).innerHTML = ``;
+    // document.getElementById(playerOne.tile.toString()).appendChild(playerOne.playerImg); // WTF 
+    document.getElementById('tile_' + whichPlayer.tile).innerHTML -= `<p>""</p><img src="${whichPlayer.playerImg}" width="60px"/>`;
     whichPlayer.tile += diceRoll;
     document.getElementById('tile_' + whichPlayer.tile).innerHTML += `<img src="${whichPlayer.playerImg}" width="60px"/>`;
     if (whichPlayer === playerOne) {
@@ -65,11 +67,12 @@ function boardFunction() {
     }
   }
 
-  // Switch statement setting the traps. 
+  // Switch statement setting the traps. WTF
   switch (whichPlayer.tile) {
     case trapOne.tileNum:
       trapOne.tileNum - trapOne.penalty;
       window.alert(trapOne.alertMessage);
+      console.log("hei")
       break;
 
     case trapTwo.tileNum:
@@ -95,93 +98,17 @@ function boardFunction() {
     default:
   }
 
+
+  // WTFF -->
+
+  // to set tile 30 to a winning tile using a for loop that iterates through the tiles. 
+  // let boardTile;
+
+  // for (i = 0; i < boardTile.length; i++) {
+  //   if (tile === 30) {
+  //     window.alert("Congratulations you won the Throne!")
+  //   }
+  // }
+
 }
 
-
-
-
-
-
-// // Gets information from session storage.
-// let playerOneImage = sessionStorage.getItem("player1");
-// let playerTwoImage = sessionStorage.getItem("player2");
-
-// // Displaying player information in the current tile they are placed in. 
-// console.log(sessionStorage.getItem("player1"));
-// console.log(sessionStorage.getItem("player2"));
-
-
-
-
-
-// // This function rolls the dice and moces the player. 
-// function rollDiceAndMovePlayer() {
-
-//   // Making a variable currentPlayer = and getting the function
-//   let currentPlayer = getCurrentPlayer();
-//   // This will display what player it is in session storage
-//   console.log("The current player is: " + currentPlayer.id);
-
-//   let diceRoll = rollDice();
-
-//   movePlayer(diceRoll, currentPlayer);
-
-//   //switching characters
-//   currentPlayer = changeTurn(currentPlayer);
-// }
-
-
-// // finds out if player one or player two is the current player
-// // returns the current player 
-// function getCurrentPlayer() {
-//   let currentPlayer = {};
-//   if (playerOne.isPlayerTurn === true) {
-//     currentPlayer = playerOne;
-//   }
-//   else {
-//     currentPlayer = playerTwo;
-//   }
-//   return currentPlayer;
-// }
-
-
-// function rollDice() {
-//   // Makes a variable named diceRoll. Contains a method that makes the dice span from 1-6 (whole numbers ->floor). 
-//   let diceRoll = (Math.floor(Math.random() * 6) + 1)
-//   console.log("The number of steps is: " + diceRoll);
-//   return diceRoll;
-// }
-
-
-// function movePlayer(diceRoll, currentPlayer) {
-//   // Change the players turn back when the dice rolls 6. If not rolled 6, display the player on the tile. 
-//   if (diceRoll === 6) {
-//     // moves 6 
-//     document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}" class="hide" width="60px" border-radius="50px"/>`;
-//     console.log('Have another roll!');
-//     diceRoll = rollDice();
-//     movePlayer(diceRoll, currentPlayer)
-//   }
-//   else {
-//     document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}" class="hide" width="60px" border-radius="50px"/>`;
-//   }
-//   // Gets the objects and tile and updates the tile with the result of the diceRoll. 
-//   currentPlayer.tile = currentPlayer.tile + diceRoll;
-//   console.log(currentPlayer.tile);
-
-//   // Placing the information from object inside the HTML. Then adds the image gotten from the previous page. 
-//   document.getElementById('tile_' + currentPlayer.tile).innerHTML += `<img src="${currentPlayer.playerImg}">`
-// }
-
-
-
-// function changeTurn(currentPlayer) {
-//   if (currentPlayer === playerOne) {
-//     playerOne.isPlayerTurn = false;
-//     playerTwo.isPlayerTurn = true;
-//   } else {
-//     playerOne.isPlayerTurn = true;
-//     playerTwo.isPlayerTurn = false;
-//   }
-//   return currentPlayer;
-// }
